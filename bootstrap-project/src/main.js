53311ccc6387f0c28161ec62ff626f2030e5950a
Israelamat <israelamatgil@gmail.com>
2026-01-02 16:39:39 +0100

    ScrollSpy and Accordion + modals

--- bootstrap-project/src/main.js
+++ bootstrap-project/src/main.js
@@ -7,3 +7,6 @@ function lightDark(){
   html.setAttribute("data-bs-theme", esOscuro ? "light" : "dark");
   theme.textContent = esOscuro ? "🌙" : "☀️";
 }
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
