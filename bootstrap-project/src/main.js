2465f944ec178b674a7ec3cb6000b4741ec9e38c
Israelamat <israelamatgil@gmail.com>
2026-01-04 02:36:15 +0100

    Se añade la inicialización de ScrollSpy dentro del evento shown.bs.tab para el tab #scrollspy-tab. Esto asegura que el ScrollSpy calcule correctamente las posiciones de los elementos cuando el contenedor está visible, ya que el scroll no se encuentra en el <body> sino en un div interno.

--- bootstrap-project/src/main.js
+++ bootstrap-project/src/main.js
@@ -1,8 +1,19 @@
 const theme = document.querySelector(".dark-light");
 const html = document.documentElement;
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+document.querySelector('#scrollspy-tab').addEventListener('shown.bs.tab', function() {
+  var scrollContainer = document.querySelector('[data-bs-spy="scroll"]');
+  new bootstrap.ScrollSpy(scrollContainer, {
+    target: '#menuViajes'
+  });
+});
+
+
 theme.addEventListener("click", lightDark);
-function lightDark(){  
+function lightDark() {
   const esOscuro = html.getAttribute("data-bs-theme") === "dark";
   html.setAttribute("data-bs-theme", esOscuro ? "light" : "dark");
   theme.textContent = esOscuro ? "🌙" : "☀️";
